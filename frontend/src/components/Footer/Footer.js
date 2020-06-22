fb19edb6f9d09efd931606fdcff50c2b03980ec6
Eleanor Alrabea <eleanor.alrabea@gmail.com>
2020-06-22 20:44:55 +0200

    Improve Footer design

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -25,25 +25,15 @@ class Footer extends React.Component {
                                 <strong> Noor Alrabea</strong>
                             </a>
                         </p>
-
-                        {/* <p>
-                            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-                        </p> */}
                     </div>
                 </footer>
-                <nav className="navbar is-light">
-                    <div className="navbar-start">
-                        <div className="navbar-menu is-size-7 paddin-left-right-2rem">
-                            <div className="navbar-item is-size-7">mPat</div>
-                            <a className="navbar-item">Terms</a>
-                            <a className="navbar-item">Privacy</a>
-                            <a className="navbar-item">What is mPat?</a>
-                        </div>
-                    </div>
-                </nav>
-            </div> 
+                <ul id="footer-menu">
+                    <li className="footer-menu-item">mPat</li>
+                    <li className="footer-menu-item">Terms</li>
+                    <li className="footer-menu-item">Privacy</li>
+                    <li className="footer-menu-item">What is mPat?</li>
+                </ul>
+            </div>
         );
     }
 }
